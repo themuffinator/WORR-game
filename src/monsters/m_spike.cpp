5917f7b3813b35cd535305f5860a1b9693f351fe
themuffinator <djdacosta86@yahoo.co.uk>
2025-10-30 00:51:48 +0000

    Fix zombie and spike macro usage and spawn temp

--- src/monsters/m_spike.cpp
+++ src/monsters/m_spike.cpp
@@ -151,10 +151,10 @@ static MonsterFrame spike_frames_pain[] = {
 };
 MMOVE_T(spike_move_pain) = { FRAME_spike1, FRAME_spike9, spike_frames_pain, spike_run };
 
-static PAIN(spike_pain) (gentity_t* self, gentity_t* other, float kick, int damage, const MeansOfDeath& mod) -> void {
+PAIN(spike_pain) (gentity_t* self, gentity_t* other, float kick, int damage, const MeansOfDeath& mod) -> void {
         if (spike_pain_is_nailgun(mod.id)) {
                 const Vector3 point = other ? other->s.origin : self->s.origin;
-                Damage(self, self, self, Vector3{}, point, Vector3{}, self->health + 10, 0, DamageFlags::None,
+                Damage(self, self, self, Vector3{}, point, Vector3{}, self->health + 10, 0, DamageFlags::Normal,
                         MeansOfDeath{ ModID::Explosives });
                 return;
         }
@@ -214,7 +214,7 @@ static void spike_explode(gentity_t* self) {
         spike_finish_explode(self);
 }
 
-static DIE(spike_die) (gentity_t* self, gentity_t* inflictor, gentity_t* attacker, int damage, const Vector3& point,
+DIE(spike_die) (gentity_t* self, gentity_t* inflictor, gentity_t* attacker, int damage, const Vector3& point,
         const MeansOfDeath& mod) -> void {
         if (self->deadFlag)
                 return;
@@ -267,8 +267,6 @@ void SP_monster_spike(gentity_t* self) {
         }
 
         spike_precache();
-
-        const spawn_temp_t& st = ED_GetSpawnTemp();
 
         self->className = "monster_spike";
         self->moveType = MoveType::Step;
