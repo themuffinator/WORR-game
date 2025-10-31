3981e036ac30d4395fe13595d8e5bfd6df25034b
themuffinator <djdacosta86@yahoo.co.uk>
2025-10-31 21:44:50 +0000

    Refactor server command module includes

---
+++ src/server/commands/commands.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "command_system.hpp"
+#include "command_registration.hpp"
+#include "command_voting.hpp"
+#include "command_voting_utils.hpp"
+
+namespace server::commands {
+
+using ::CommandArgs;
+using ::CommandFlag;
+using ::RegisterCommand;
+
+using namespace ::Commands;
+
+} // namespace server::commands
+
