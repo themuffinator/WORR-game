5d2394290e341765762623a39de38dd629a19407
themuffinator <djdacosta86@yahoo.co.uk>
2025-11-08 13:01:37 +0000

    Add generated version header support

---
+++ src/shared/version.hpp
@@ -0,0 +1,9 @@
+// This file is generated. Do not edit directly.
+#pragma once
+
+#define WORR_VERSION_MAJOR 0
+#define WORR_VERSION_MINOR 1
+#define WORR_VERSION_PATCH 0
+#define WORR_VERSION_PRERELEASE "alpha"
+#define WORR_BUILD_METADATA "20240315"
+#define WORR_SEMVER "0.1.0-alpha+20240315"
