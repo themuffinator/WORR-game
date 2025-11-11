df43483278646ae6deae891e89c6b8ebee7b170e
themuffinator <djdacosta86@yahoo.co.uk>
2025-11-11 11:42:34 +0000

    update Head hunters spawn_name to "hh"

--- src/server/g_local.hpp
+++ src/server/g_local.hpp
@@ -263,7 +263,7 @@ constexpr std::array<GameTypeInfo, static_cast<size_t>(GameType::Total)> GAME_MO
 	{GameType::LastManStanding,"lms",      "LMS",      "Last Man Standing",   "lms",		GameFlags::Elimination},
 	{GameType::LastTeamStanding,"lts",     "LTS",      "Last Team Standing",  "lts",		GameFlags::Teams | GameFlags::Elimination},
 	{GameType::Horde,          "horde",    "HORDE",    "Horde Mode",          "horde",		GameFlags::Rounds},
-	{GameType::HeadHunters,    "hh",       "HH",       "Head Hunters",        "headhunters",		GameFlags::None},
+	{GameType::HeadHunters,    "hh",       "HH",       "Head Hunters",        "hh",			GameFlags::None},
 	{GameType::ProBall,        "ball",     "BALL",     "ProBall",             "ball",		GameFlags::Teams},
 	{GameType::Gauntlet,       "gauntlet", "GAUNTLET", "Gauntlet",            "gauntlet",	GameFlags::OneVOne | GameFlags::Rounds | GameFlags::Frags}
 } };
